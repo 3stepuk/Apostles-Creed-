e79c01aae9e7cc218a7c98a87258b3bfa657004b
Mark Harris <mark@3step.uk>
2026-09-04 16:27:33 +0100

    Set base path / build

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/Apostles-Creed-/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
